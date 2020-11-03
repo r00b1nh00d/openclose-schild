0d6ae6c2ea17f3431cf8513577255be33be37c1b
r00b1nh00d <60343132+r00b1nh00d@users.noreply.github.com>
2020-11-03 13:12:57 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,1 @@
-let Schild = ""
-input.onButtonPressed(Button.A, function () {
-    Schild = "Open"
-})
-input.onButtonPressed(Button.B, function () {
-    Schild = "Close"
-})
-basic.forever(function () {
-    basic.showString(Schild)
-})
-basic.forever(function () {
-    basic.showString("open")
-})
+
